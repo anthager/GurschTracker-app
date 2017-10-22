a61ab6bde09b08d90e58e03a132571a5e7cd9e09
antonhager <anthage@student.chalmers.se>
2017-10-22 21:32:56 +0200

    It's now possible to go to the overview from the login

--- GurschTracker/LoginViewController.swift
+++ GurschTracker/LoginViewController.swift
@@ -21,19 +21,33 @@ class LoginViewController: UIViewController, AuthValidation {
 
 	}
 
-	private func login() {
+	//Some nice loading is needed here, and fail shit
+	@IBAction func signInButtonPressed(_ sender: UIButton) {
+	//	login()
+		let storyboard = UIStoryboard(name: "Main", bundle: nil)
+		let controller = storyboard.instantiateInitialViewController()
+
+		self.present(controller!, animated: true, completion: nil)
+	}
+
+	private func login() -> Bool {
 		guard let email = emailTextField.text, isValidEmail(emailTextField.text) else {
 			print("invalid email entered")
-			return
+			return false
 		}
 		guard let password = passwordTextField.text, isAValidPassword(passwordTextField.text) else {
 			print("invalid password entered")
-			return
+			return false
 		}
+		var	success = false
 		Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
 			if error != nil {
 				print(error as Any)
 			}
+			else {
+				success = true
+			}
 		}
+		return success
 	}
 }
